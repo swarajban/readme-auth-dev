843172116a7e30c176c5cba27df1c6b66134dd6b
Swaraj Banerjee <swarajban@gmail.com>
2016-03-22 17:01:18 -0700

    added unit test for readme-auth

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var jsonwebtoken = require('jsonwebtoken');
 var uuid = require('node-uuid');
 
-exports.default = function (readmeURL, jwtSecret) {
+module.exports = function (projectURL, jwtSecret) {
   return {
+
+    // Return an URL with an authentication token in the query string
     getAuthUrl: function (payload) {
+      var jwt = this._getJWT(payload, jwtSecret);
+      return projectURL + '?auth_token=' + jwt;
+    },
+
+    // Internal function for generating JWTs
+    _getJWT: function (payload, secret) {
       var jwtOptions = {
-        jwtid: uuid.v4()
+        jwtid: uuid.v4(),
+        audience: this.audience
       };
-      var jwt = jsonwebtoken.sign(payload, jwtSecret, jwtOptions);
-      return readmeURL + '?auth_token=' + jwt;
-    }
+      return jsonwebtoken.sign(payload, secret, jwtOptions);
+    },
+
+    // Audience for JWT
+    audience: 'readme.io'
+
   };
 };
--- test/test.js
+++ test/test.js
@@ -1,1 +1,64 @@
 var assert = require('assert');
+var jsonwebtoken = require('jsonwebtoken');
+
+var ReadMeAuth = require('../index');
+
+describe('ReadMe Auth', function () {
+
+  var TestAuth;
+  var JWT_SECRET = 'jwts3cr3t';
+  var READ_ME_PROJECT_URL = 'readme-auth.readme.io';
+
+  var TEST_PAYLOAD = {
+    name: 'Owlbert',
+    email: 'owlbert@readme.io'
+  };
+
+  // Initialize a ReadMeAuth client
+  before(function () {
+    TestAuth = ReadMeAuth(READ_ME_PROJECT_URL, JWT_SECRET);
+  });
+
+  // Test generating a valid JWT with given payload & secret
+  describe('_getJWT', function () {
+    it('should return a valid JWT when given a payload', function (done) {
+      var token = TestAuth._getJWT(TEST_PAYLOAD, JWT_SECRET);
+      jsonwebtoken.verify(token, JWT_SECRET, { audience: TestAuth.audience },
+        function (err, decoded) {
+          assert.ifError(err);
+          assert.ok(decoded);
+          assert.ok(decoded.jti);
+          assert.equal(TEST_PAYLOAD.name, decoded.name);
+          assert.equal(TEST_PAYLOAD.email, decoded.email);
+          done();
+        }
+      );
+    });
+  });
+
+  // Test getting full auth URL using this readme-auth module
+  describe('getAuthUrl', function () {
+    it('should return an url with a valid JWT as a query param', function (done) {
+      var url = TestAuth.getAuthUrl(TEST_PAYLOAD);
+      var parts = url.split('?');
+      assert.equal(2, parts.length);
+      var projectURL = parts[0];
+      assert.equal(READ_ME_PROJECT_URL, projectURL);
+      var queryParams = parts[1];
+      assert.equal(READ_ME_PROJECT_URL, parts[0]);
+      var queryParts = queryParams.split('=');
+      var queryKey = queryParts[0];
+      var queryToken = queryParts[1];
+      assert.equal('auth_token', queryKey);
+
+      jsonwebtoken.verify(queryToken, JWT_SECRET,
+        function (err) {
+          assert.ifError(err);
+          done();
+        }
+      );
+    });
+
+  });
+
+});
